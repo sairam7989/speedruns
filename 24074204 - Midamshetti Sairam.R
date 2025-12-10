522ed261593ff1542ed269cb00bd338960333a9e
sairam7989 <sm24akp@herts.ac.uk>
2025-12-10 16:27:30 +0530

    Loaded and read Data

--- 24074204 - Midamshetti Sairam.R
+++ 24074204 - Midamshetti Sairam.R
@@ -8,3 +8,13 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 
+# Loading all CSV Data
+file_list <- list.files(pattern = "*.csv")
+
+# Read each file and adding a 'category' column based on filename
+data_list <- lapply(file_list, function(file) {
+  df <- read.csv(file)
+  category_name <- gsub("data_", "", gsub(".csv", "", file))
+  df$category <- category_name
+  return(df)
+})
